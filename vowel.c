3834783eee7b0362b3ccf10e00ddae7b6b440ad5
sunilkumarbysani <51699442+sunilkumarbysani@users.noreply.github.com>
2019-07-05 21:58:50 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -6,7 +6,7 @@ int main()
     char c;
     scanf("%c",&c);
     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
-    printf("vowel",c);
+    printf("Vowel",c);
     else
      printf("Consonant",c);
     
